c71708b135f6b427b7d3a60707da3f63fd33ed30
Tanvir Ahmed Talukder <rahidt78@gmail.com>
2024-03-04 12:07:34 +0600

    Build Way section.

--- src/components/section/Way.jsx
+++ src/components/section/Way.jsx
@@ -1,3 +1,19 @@
+import { SectionImage, Image, TextWrapper, H2, P, Button } from "../utils";
+import { sectionImage4 } from "../../assets";
+
 export function Way() {
-  return <div>Way</div>;
+  return (
+    <SectionImage className="relative justify-center ">
+      <Image src={sectionImage4}></Image>
+      <TextWrapper className="items-center absolute right-40 bottom-1/3">
+        <H2 className="mb-1">The ways we work</H2>
+        <P className="text-sm opacity-100">
+          How has our relationship with work has changed
+        </P>
+        <Button size="w-full max-w-36" className="bg-blue-500 text-xs mt-2">
+          View the report
+        </Button>
+      </TextWrapper>
+    </SectionImage>
+  );
 }
